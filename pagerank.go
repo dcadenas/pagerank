a09a7736bf87920a3d819debfc28974735af216b
Daniel Cadenas <dcadenas@gmail.com>
2013-06-20 16:50:54 -0300

    Initialize pageRank struct

--- pagerank.go
+++ pagerank.go
@@ -7,16 +7,26 @@ type Interface interface {
 }
 
 type pageRank struct {
+  inLinks [][]int
+  numberOutLinks []int
+  currentAvailableIndex int
+  keyToIndex map[int]int
+  indexToKey map[int]int
 }
 
 func New() *pageRank {
   rg := new(pageRank)
+  rg.inLinks = [][]int{}
+  rg.numberOutLinks = []int{}
+  rg.currentAvailableIndex = -1
+  rg.keyToIndex = make(map[int]int)
+  rg.indexToKey = make(map[int]int)
   return rg;
 }
 
-func (v *pageRank) Link(from, to int) {
+func (pr *pageRank) Link(from, to int) {
 }
 
-func (v *pageRank) Rank(followingProb, tolerance Float, resultFunc func(label int, rank Float)) {
+func (pr *pageRank) Rank(followingProb, tolerance Float, resultFunc func(label int, rank Float)) {
   resultFunc(1, 1.0)
 }
